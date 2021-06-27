32569dfeec2a28b8bc7089210520a5c950659896
Shourya Shikhar <danger.ahead@pm.me>
2021-06-27 21:06:15 +0530

    added-texteditingcontroller

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 dynamic homeLayout(int selected) {
+
+    TextEditingController _word = TextEditingController();
+
     List<Widget> widgetArray = [
-        Text('test1'),
+        Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 25),
+            child: Column(
+                children: [
+                    TextField(
+                        controller: _word,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter word',
+                            suffixIcon: Icon(Icons.search)
+                        ),  //InputDecoration
+                    ),  //TextField
+                    ElevatedButton(
+                        onPressed: (){
+                            print(_word.text);
+                        },
+                        child: Text('Find meaning!')
+                    )
+                ],  //Column-children
+            )   //Column
+        ),  //Padding
         Text('test2')
     ];
     return widgetArray[selected];
