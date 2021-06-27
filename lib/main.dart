b3fde07e7788dfe501406d201a787bf82b2afbb5
Shourya Shikhar <danger.ahead@pm.me>
2021-06-27 17:51:50 +0530

    bottomNavBar-added

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  int selected = 0;
+  List<Widget> widgetArray = [
+    Text('test'),
+    Text('test2')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,10 +48,10 @@ class _MyHomePageState extends State<MyHomePage> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children:[
                               Text(
-                                "D I C T I O N A R Y   ",
+                                "D I C T I O N A R Y ",
                                 style: TextStyle(color: Colors.white, fontSize: 20),
                               ),
-                              Image.asset('images/icon.png', height: 20, width: 20,)
+                              Image.asset('images/icon.png', height: 20, width: 20)
                             ],  //row-children
                           ),
                       )
@@ -55,16 +61,30 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
                 padding: const EdgeInsets.only(top: 100),
                 child: Container(
+                  child: Center(
+                    child: widgetArray[selected],
+                  ),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15))
-                  )
+                    topRight: Radius.circular(15)),
+                  ),
                 )
             )
           ],
-        )
-
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+            BottomNavigationBarItem(icon: Icon(Icons.account_balance_outlined), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.article_outlined), label: 'About'),
+          ],
+          onTap: tapBottomNavBar,
+          currentIndex: selected,
+        ),
     );
+  }
+  void tapBottomNavBar(int integer) {
+    setState(() {
+      selected = integer;
+    });
   }
 }
