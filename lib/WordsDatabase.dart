31e9fc7e10e3f4935e46d8f5dee2b6ef8a8f59f5
Shourya Shikhar <danger.ahead@pm.me>
2021-07-21 21:38:56 +0530

    started-work-on-previous-words-db

---
+++ lib/WordsDatabase.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_dictionary/words.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class WordsDatabase{
+  static final WordsDatabase instance = WordsDatabase._init();
+
+  static Database? _database;
+
+  WordsDatabase._init();
+
+  Future<Database> get database async{
+    if (_database != null) return _database;
+
+    _database = await _initDB('words.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async{
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async{
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final wordType = 'TEXT NOT NULL';
+
+    await db.execute('''
+    CREATE TABLE $tableWords(
+      ${WordFields.id} $idType,
+      ${WordFields.word} $wordType
+    )
+    ''');
+  }
+  
+  Future<Word> create(Word word) async{
+    final db = await instance.database;
+
+    final id = await db.insert(tableWords, word.toJson());
+    return word.copy(id:id);
+  }
+
+  Future<List<Word>> readAllNotes() async{
+    final db = await instance.database;
+
+    final orderBy = '${WordFields.id} ASC';
+
+    final result = await db.query(tableWords, orderBy: orderBy);
+
+    return result.map((json) => Word.fromJson(json)).toList();
+  }
+
+  Future<Word?> readWord(int id) async{
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableWords,
+      columns: WordFields.values,
+      where: '${WordFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty){
+      return Word.fromJson(maps.first);
+    } else{
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future close() async{
+    final db = await instance.database;
+
+    db.close();
+  }
+}
---
+++ lib/words.dart
@@ -0,0 +1,39 @@
+final String tableWords = 'words';
+
+class WordFields{
+  static final List<String> values = [
+    id, word
+  ];
+
+  static final String id = '_id';
+  static final String word = 'word';
+}
+
+class Word{
+  final int? id;
+  final String word;
+
+  const Word({
+    this.id,
+    required this.word,
+  });
+
+  Word copy({
+    int? id,
+    String? word,
+  }) =>
+      Word(
+        id: id ?? this.id,
+        word: word ?? this.word,
+      );
+
+  static Word fromJson(Map<String, Object?> json) => Word(
+    id: json[WordFields.id] as int?,
+    word: json[WordFields.word] as String,
+  );
+
+  Map<String, Object?> toJson() => {
+    WordFields.id: id,
+    WordFields.word: word,
+  };
+}
