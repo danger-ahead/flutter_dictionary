7d2d422d3b7a74112d88957bd42dd6c437cac255
Abh1noob <abhinavganeshank@gmail.com>
2023-10-03 01:20:18 +0530

    Themes implemented based on device mode, Button to be implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dictionary/constants/strings.dart';
+import 'package:flutter_dictionary/themes/dark_theme.dart';
+import 'package:flutter_dictionary/themes/light_theme.dart';
 import 'package:flutter_dictionary/ui/random_word.dart';
 import 'package:flutter_dictionary/ui/results.dart';
 import 'package:flutter_dictionary/ui/home.dart';
@@ -15,9 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
       initialRoute: StringConstants.homeRoute,
       routes: {
         StringConstants.homeRoute: (context) => Home(),
